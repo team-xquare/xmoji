bb4f02c0f1190522a4d94f897c7a602c191821dd
smoothbear <kjbin0420@naver.com>
2021-05-26 10:06:42 +0900

    :coffin: :: (#1) [index.js] 테스트 주석 삭제

--- src/commands/commit/withClient/index.js
+++ src/commands/commit/withClient/index.js
@@ -6,7 +6,6 @@ import isHookCreated from '../../../utils/isHookCreated'
 import configurationVault from '../../../utils/configurationVault'
 import { type Answers } from '../prompts'
 
-//
 const withClient = async (answers: Answers) => {
   try {
     const scope = answers.scope ? `(${answers.scope}): ` : ''
